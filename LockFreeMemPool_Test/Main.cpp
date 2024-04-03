ed391d93e73c7c5acf3e6958fb56f0fb73b19636
JinHur <jinh2352@gmail.com>
2024-04-03 16:42:30 +0900

    락-프리 메모리 풀 테스트 코드

--- LockFreeMemPool_Test/Main.cpp
+++ LockFreeMemPool_Test/Main.cpp
@@ -1,16 +1,76 @@
 #include "LockFreeMemPool.h"
+#include <process.h>
+#include <iostream>
 
 #pragma comment(lib, "LockFreeMemoryPool")
 
-int main() {
-	LockFreeMemPool lfmp(sizeof(int), 4);
-	int* ptr = (int*)lfmp.Alloc();
-	int* ptr1 = (int*)lfmp.Alloc();
-	int* ptr2 = (int*)lfmp.Alloc();
-	int* ptr3 = (int*)lfmp.Alloc();
+#define NUM_OF_THREADS	8
+#define TEST_LOOP		10000
+#define NUM_OF_LOOP		100
+#define NUM_OF_PTR		100
 
-	lfmp.Free(ptr2);
-	lfmp.Free(ptr3);
-	lfmp.Free(ptr);
-	lfmp.Free(ptr1);
+//LockFreeMemPool g_LFMP(sizeof(int), 0);
+LockFreeMemPool g_LFMP(sizeof(int), NUM_OF_THREADS * NUM_OF_PTR);
+bool errorFlag = false;
+
+unsigned WINAPI TestJob(void* arg) {
+	int* ptrArr[NUM_OF_PTR + 1];
+
+	for (int k = 0; k < NUM_OF_LOOP; k++) {
+		for (int incre = 1; incre <= NUM_OF_PTR; incre++) {
+
+			// Alloc
+			for (int i = 0; i < incre; i++) {
+				ptrArr[i] = (int*)g_LFMP.Alloc();
+				int initVal = *ptrArr[i];
+				if (initVal != 0xFDFDFDFD) {
+					errorFlag = true;
+					DebugBreak();
+					return 1;
+				}
+				if (i > 0 && ptrArr[i] == ptrArr[i - 1]) {
+					//LogVectorCheck();
+					errorFlag = true;
+					DebugBreak();
+					return 1;
+				}
+				*ptrArr[i] = i;
+			}
+
+			// Free
+			for (int i = 0; i < incre; i++) {
+				if (*ptrArr[i] != i) {
+					errorFlag = true;
+					DebugBreak();
+					return 1;
+				}
+
+				g_LFMP.Free(ptrArr[i]);
+			}
+		}
+	}
+	return 0;
+}
+
+int main() {
+	for (int i = 0; i < TEST_LOOP; i++) {
+		InitLog();
+		HANDLE hThreads[NUM_OF_THREADS];
+		for (int i = 0; i < NUM_OF_THREADS; i++) {
+			hThreads[i] = (HANDLE)_beginthreadex(NULL, 0, TestJob, NULL, 0, NULL);
+		}
+
+		WaitForMultipleObjects(NUM_OF_THREADS, hThreads, TRUE, INFINITE);
+
+		if (errorFlag) {
+			LogVectorCheck();
+		}
+
+		for (int i = 0; i < NUM_OF_THREADS; i++) {
+			CloseHandle(hThreads[i]);
+		}
+
+		std::cout << "Test..." << std::endl;
+	}
+	return 0;
 }
